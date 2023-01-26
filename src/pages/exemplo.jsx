8af49f657b6c8978079c3262449b434dcbb48ffc
Marcio Bueno <mabuen@live.com>
2023-01-26 10:55:41 +0100

    propriedades dos componentes

---
+++ src/pages/exemplo.jsx
@@ -0,0 +1,10 @@
+import Cabecalho from "../components/Cabecalho"
+
+export default function Exemplo(){
+  return(
+    <>
+      <Cabecalho titulo="Fundamentos do Next.js & React"></Cabecalho>
+      <Cabecalho titulo="Aprenda Next na prática"></Cabecalho>
+    </>
+  )
+}
